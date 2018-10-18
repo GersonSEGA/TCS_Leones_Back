eeaa80bd7961a1dadf43bf6a950933329fc46786
GersonSEGA <g.sanchezesparza@gmail.com>
2018-10-18 01:23:39 -0500

    commit

--- src/main/java/edu/moduloalumno/api/AlumnoProgramaController.java
+++ src/main/java/edu/moduloalumno/api/AlumnoProgramaController.java
@@ -90,7 +90,6 @@ public class AlumnoProgramaController {
 	}
 	
 	
-	//FUCCCKKKKK
 	@RequestMapping(value = "/buscarc/{codAlumno}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<AlumnoPrograma>> getAlumnoProgramaByCodAlumno(@PathVariable("codAlumno") String codAlumno) {
 		logger.info("> getAlumnoProgramaByCodAlumno [AlumnoPrograma]");
